94ba6775ed29362dccc338c2be88cbd55c0a1f80
himika <clover@himika.com>
2016-01-30 18:59:14 +0900

    Updated TESTopicInfo

--- Skyrim/include/Skyrim/Forms/TESTopicInfo.h
+++ Skyrim/include/Skyrim/Forms/TESTopicInfo.h
@@ -3,6 +3,8 @@
 #include "TESForm.h"
 #include "../FormComponents/Condition.h"
 
+class TESTopic;
+
 /*==============================================================================
 class TESTopicInfo +0000 (_vtbl=010A6CCC)
 0000: class TESTopicInfo
@@ -15,7 +17,7 @@ class TESTopicInfo : public TESForm
 public:
 	enum { kTypeID = (UInt32)FormType::TopicInfo };
 
-	enum
+	enum DialogFlag
 	{
 		kDialogFlag_Goodbye =					1 << 0,
 		kDialogFlag_Random =					1 << 1,
@@ -29,7 +31,23 @@ public:
 		kDialogFlag_SpendsFavorPoints =			1 << 14,
 	};
 
-	UInt32		unk14;				// 14
+	virtual ~TESTopicInfo();		// 0057ED90
+
+	// @override
+	virtual void	Unk_04(void) override;									// 04 0057E4E0
+	virtual void	Unk_05(void) override;									// 05 0057E580
+	virtual bool	LoadForm(TESFile *mod) override;						// 06 0057E8F0
+	virtual void	LoadBuffer(BGSLoadFormBuffer *buf) override;			// 0F 0057D4D0
+	virtual void	Unk_12(BGSLoadFormBuffer *buf) override;				// 12 0057D4F0
+	virtual void	InitItem(void) override;								// 13 0057E5D0
+	virtual void	GetFormDesc(char * buf, UInt32 bufLen) override;		// 16 004D43E0 { return; }
+	virtual void	SetFlag00000002(bool set) override;						// 24 0057D430
+	virtual bool	Unk_30(UInt32 arg0, UInt32 arg1, UInt32 arg2) override;	// 30 0057D320
+	virtual bool	Unk_31(void * dst, UInt32 unk) override;				// 31 0057D470
+
+
+	// @members
+	TESTopic	* topic;			// 14
 	UInt32		unk18;				// 18 - show response data from info?
 	Condition	* conditions;		// 1C - linked list
 	UInt16		unk20;				// 20 - init'd to FFFF
@@ -37,6 +55,9 @@ public:
 	UInt8		favorLevel;			// 23
 	UInt16		dialogFlags;		// 24
 	UInt16		hoursUntilReset;	// 26
-	void		* unk28;			// 28 - quest pointer?
+	UInt32		unk28;				// 28
+private:
+	DEFINE_MEMBER_FN(ctor, TESTopicInfo *, 0x0057ECC0);
+	DEFINE_MEMBER_FN(dtor, void, 0x0057ECF0);
 };
 STATIC_ASSERT(sizeof(TESTopicInfo) == 0x2C);
